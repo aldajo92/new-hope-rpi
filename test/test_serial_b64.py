a645d7af1424c89d284774c4f58c63b90ca50471
algomez <algomez@hugeinc.com>
2017-01-13 16:57:23 -0500

    change baud rate

---
+++ test/test_serial_b64.py
@@ -0,0 +1,22 @@
+import serial
+import time
+
+PORT = "/dev/ttyAMA0"
+BAUD = 9600
+
+
+def connect():
+    return serial.Serial(PORT, BAUD)
+
+
+serial_port = connect()
+
+try:
+    while True:
+        serial_port.write("hello" + "\r\n")
+        # print "hello"
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print "closing..."
+    exit(0)
