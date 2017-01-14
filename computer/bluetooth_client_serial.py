758a983bc3b4614b3c5c4bf0e4895d082d94e0f2
algomez <algomez@hugeinc.com>
2017-01-14 00:36:13 -0500

    adding raspberry code and client bluetooth

---
+++ computer/bluetooth_client_serial.py
@@ -0,0 +1,42 @@
+import threading
+import serial
+import time
+
+PORT = "/dev/tty.HC-05-DevB"
+BAUD = 460800
+serial_port = None
+
+thread = None
+
+
+def read_from_port():
+    while True:
+        reading = serial_port.readline()
+        if reading != "" and reading != " ":
+            print reading
+
+
+def main():
+    global thread
+    thread = threading.Thread(target=read_from_port)
+    thread.start()
+
+
+try:
+    while True:
+        try:
+            serial_port = serial.Serial(PORT, BAUD)
+        except serial.SerialException:
+            print "trying..."
+            time.sleep(2)
+            continue
+        break
+
+    print "connected..."
+    serial_port.flushInput()
+    serial_port.flushOutput()
+    main()
+
+except KeyboardInterrupt:
+    print "clean"
+    exit(0)
---
+++ rpi/buttons_pressed.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+
+INPUT_1 = 11
+INPUT_2 = 12
+INPUT_3 = 13
+INPUT_4 = 15
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(INPUT_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(INPUT_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(INPUT_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(INPUT_4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+def my_callback_1(channel):
+    print "falling edge detected on 17"
+
+
+def my_callback_2(channel):
+    print "falling edge detected on 23"
+
+
+def my_callback_3(channel):
+    print "falling edge detected on 23"
+
+
+def my_callback_4(channel):
+    print "falling edge detected on 23"
+
+
+GPIO.add_event_detect(INPUT_1, GPIO.RISING, callback=my_callback_1, bouncetime=300)
+GPIO.add_event_detect(INPUT_2, GPIO.RISING, callback=my_callback_2, bouncetime=300)
+GPIO.add_event_detect(INPUT_3, GPIO.RISING, callback=my_callback_3, bouncetime=300)
+GPIO.add_event_detect(INPUT_4, GPIO.RISING, callback=my_callback_4, bouncetime=300)
+
+try:
+    print "press something"
+except KeyboardInterrupt:
+    GPIO.cleanup()  # clean up GPIO on CTRL+C exit
+GPIO.cleanup()  # clean up GPIO on normal exit
